a188460997cb3ec168275d31cedccf0bf37be183
FrackenToaster <frakentoaster@gmail.com>
2015-08-06 20:51:45 -0700

    Mark as complete ability added

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -16,6 +16,11 @@ class TodoItemsController < ApplicationController
 		redirect_to @todo_list
 	end
 
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "Todo item completed."
+	end
+
 	private
 
 	def set_todo_list
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :todo_lists do 
-    resources :todo_item 
+
+  resources :todo_lists do
+    resources :todo_items do
+    	member do
+    		patch :complete
+    	end
+    end
   end
-  
+
   root "todo_lists#index"
 end
